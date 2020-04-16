8ef3938bbacb008e8e800fc937379e170d9b9dd4
mizz0224 <wosud0224@naver.com>
2020-04-16 23:45:50 +0900

    coding is so fungit add .

--- users/models.py
+++ users/models.py
@@ -5,5 +5,20 @@ from django.db import models
 
 
 class User(AbstractUser):
+    """Custom User Moded"""
 
-    pass
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICE = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICE, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
