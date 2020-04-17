5be19cfeec5f064aee3772dbdfa2fb87226430dc
mizz0224 <wosud0224@naver.com>
2020-04-17 20:12:50 +0900

    i'm so tired...

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,12 @@ from . import models
 # Register your models here.
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    """Custom User Admin"""
+
+    # admin.site.register(models.User, CustomUserAdmin)
+    list_display = ("username", "gender", "language", "currency", "superhost")
+    list_filter = (
+        "language",
+        "currency",
+        "superhost",
+    )
