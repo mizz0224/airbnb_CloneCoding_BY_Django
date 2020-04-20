92a3b4a7b32ab18b6fbf6938c8afed6fbc823d4f
mizz0224 <wosud0224@naver.com>
2020-04-20 23:30:28 +0900

    clonecoding is so funny and every commit is more fungit add .

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class TimeStampeddModel(models.Model):
+    """ Time Stamped Model"""
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
