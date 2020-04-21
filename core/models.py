6e0f8cbe61e6fd1dc00e02c3e512f82d17691462
mizz0224 <wosud0224@naver.com>
2020-04-21 16:44:30 +0900

    make by coding is so funny

--- core/models.py
+++ core/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
-# Create your models here.
-class TimeStampeddModel(models.Model):
-    """ Time Stamped Model"""
 
+class TimeStampedModel(models.Model):
+
+    """ Time Stamped Model """
+
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
--- rooms/admin.py
+++ rooms/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+@admin.register(models.RoomType)
+class ItemAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(models.Room)
